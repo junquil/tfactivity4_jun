637ad44028312f164cd0dce2da8f26e2ec0211a8
Jun Quiletorio <44885253+alfredoquiletorio@users.noreply.github.com>
2025-02-05 07:42:29 +0000

    fix naming

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "devops_jun_50646" {
+resource "aws_instance" "devopsjun741" {
     ami             = "ami-0c614dee691cbbf37"
     instance_type   = "t2.micro"
     key_name        = "devops_jun"
@@ -6,6 +6,6 @@ resource "aws_instance" "devops_jun_50646" {
     security_groups = [ aws_security_group.sg_manila_ops.name ]
 
     tags = {
-        Name = "jun_ec2_50646"
+        Name = "devopsjun741"
     }
 }
